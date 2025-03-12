7fe5a153c2dd05de41005b87c0c4a8253544bf29
Nguyễn Duy Vũ <132334654+NguyenVu04@users.noreply.github.com>
2025-03-12 23:03:22 +0700

    almost done

--- AI/Bot.py
+++ AI/Bot.py
@@ -4,7 +4,12 @@ from DeviceData import DeviceData
 from GardenInfo import GardenInfo
 import random
 from MongoConnection import MongoConnection
+from ai_model import Model
 import time
+import torch
+import torch.nn as nn
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 class Bot:
     manager: MQTTManager
@@ -18,7 +23,18 @@ class Bot:
         self.mainThread.start()
         
     def getResult(self, deviceData: DeviceData, gardenInfo: GardenInfo) -> bool:
-        return random.choice([True, False])
+        model = Model.Net()
+        model.load_state_dict(torch.load("ai_model/model.pth"))
+        model.eval()
+        input = {
+            'CropType': gardenInfo.getTreeType(),
+            'CropDays': 10,
+            'SoilMoisture': deviceData.moisture,
+            'Temperature': deviceData.temperature,
+            'Humidity': deviceData.humidity
+        }
+        
+        return Model.predict(input)
         
     def loop(self):
         while not self.stopEvent.is_set():
