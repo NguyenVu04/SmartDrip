6dab3fad5298c587f3b55be87619917acb7ffb20
Tran Ngoc Lam Vy <vy.tranngoclam@gmail.com>
2025-03-19 13:20:26 +0700

    ok

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -2,9 +2,11 @@ import axios from "axios";
 
 export async function getAPI() {
     try {
-        const response = await axios.get("10.28.129.163:8000/data/67d39586dba033df984487c9")
+        const response = await axios.get("http://localhost:8000/data/67d39586dba033df984487c9")
         console.log(response)
+        return response.data
     } catch (err) {
         console.error(err);
+        return null;
     }
 }
